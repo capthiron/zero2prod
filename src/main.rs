9172e39ec499c55f30a48a10cf11f2659eca8e37
dleitter <dario.leittersdorf@bonprix.net>
2024-11-17 00:23:00 +0100

    chore: Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,10 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
 
 async fn health_check() -> impl Responder {
