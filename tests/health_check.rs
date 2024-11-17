8fb1d606b628050dfc3e570c72256aa28c4f41c6
capthiron <24925821+capthiron@users.noreply.github.com>
2024-11-17 01:21:49 +0100

    feat: Add first integration test for health_check endpoint

---
+++ tests/health_check.rs
@@ -0,0 +1,25 @@
+//! health_check.rs
+
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::test]
+async fn health_check_works() {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind listener to random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to spawn our app.");
+    tokio::spawn(server);
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(format!("http://127.0.0.1:{}/health_check", port))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
