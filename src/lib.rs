2711dd3466583120bf2e281027d0e0de6860fe33
capthiron <24925821+capthiron@users.noreply.github.com>
2024-11-26 23:19:09 +0100

    feat: Add subscribe endpoint with initial test cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,29 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| 
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscribe", web::post().to(subscribe))
+        )
         .listen(listener)?
         .run();
 
     Ok(server)
 }
 
-async fn health_check() -> impl Responder {
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct SubscriptionFormData {
+    email: String,
+    name: String
+}
+
+async fn subscribe(_form: web::Form<SubscriptionFormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
